2d775590cf40e1489d3bdc5e49571698ff7cabad
Your Name01~ <you@example.com>
2023-02-20 23:25:40 +0100

    minitalk

--- bonus/minitalk.h
+++ bonus/minitalk.h
@@ -6,7 +6,7 @@
 /*   By: cmichez <cmichez@student.42nice.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/18 18:46:04 by cmichez           #+#    #+#             */
-/*   Updated: 2023/02/20 16:46:52 by cmichez          ###   ########.fr       */
+/*   Updated: 2023/02/20 23:24:05 by cmichez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,6 @@ void	printf_shell(int compteur);
 void	ft_putchar(char c);
 int		ft_strlen(char *str);
 void	decal_bit(unsigned char c, int pid);
-char	*ft_itoa(int n);
 int		ft_atoi(char *nptr);
 void	receive(int s);
 char	*ft_strjoin(char *s1, char s2);
--- itoa.c
+++ itoa.c
@@ -12,44 +12,6 @@
 
 #include "minitalk.h"
 
-static int	count_size(int n)
-{
-	int	i;
-
-	i = 0;
-	while (n / 10)
-	{
-		i++;
-		n /= 10;
-	}
-	return (i + 1);
-}
-
-char	*ft_itoa(int n)
-{
-	int		i;
-	char	*str;
-	long	nb;
-
-	i = count_size(n);
-	nb = n;
-	str = malloc(sizeof(char) * i + 1);
-	if (!str)
-		return (0);
-	if (n < 0)
-		nb *= -1;
-	str[i--] = '\0';
-	while (i >= 0)
-	{
-		str[i] = nb % 10 + 48;
-		nb /= 10;
-		i--;
-	}
-	if (n < 0)
-		str[0] = '-';
-	return (str);
-}
-
 int	ft_atoi(char *nptr)
 {
 	int	pair;
